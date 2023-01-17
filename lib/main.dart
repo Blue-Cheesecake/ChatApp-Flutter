aff179e85f2563c2127b2e54b1567e16efeba9b3
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-01-18 00:35:20 +0700

    feat: stream messages

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:chatapp/screens/chat/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Main());
 }
--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,20 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({Key? key}) : super(key: key);
+class ChatScreen extends StatefulWidget {
+  ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late final Stream<QuerySnapshot> _messages;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _messages = FirebaseFirestore.instance
+        .collection("chats/GkCfFuTyS8kEU7qhNOAU/messages")
+        .snapshots();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     // List of message
     return Scaffold(
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text("This work"),
-          ),
+        child: StreamBuilder(
+          stream: _messages,
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            // snapshot.data?.docs.forEach((element) {
+            //   print((element.data() as Map<String, dynamic>)['text']);
+            // });
+            // print(snapshot.hasData);
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            if (snapshot.hasError) {
+              return Text("Error");
+            }
+
+            List<QueryDocumentSnapshot> docs = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.all(8),
+                child:
+                    Text((docs[index].data() as Map<String, dynamic>)['text']),
+              ),
+            );
+          },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {},
+        child: Icon(Icons.add),
       ),
     );
   }
